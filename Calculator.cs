064c1ea921a5959229da1c00aa2d703aab853501
unknown <alfrendra.rp@students.amikom.ac.id>
2024-03-05 12:15:26 +0700

    Melengkapi tugas 4

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,15 @@ class calculator
 {
 	static void Main (string[] args)
 	{
-	
+	  int a = 10;
+	  int b = 6;
+	  
+	  Console.WriteLine(Hasil Penambahan: {0} + {1} = {2}", a,b, Penambahan(a, b));
+	  Console.WriteLine(Hasil Pengurangan: {0} - {1} = {2}", a,b, Pengurangan(a, b));
+	  
+	  Console.Writeline("\nTekan sembarang untuk keluar");
+	  Console.Readkey();
+	  
 	}
 	
 	static int Penambahan(int a, int b)
